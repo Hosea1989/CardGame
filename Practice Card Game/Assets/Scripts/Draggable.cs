2e9427fd6117fa3bbff03bf8cec209451e763078
Damien Hosea <50435897+Hosea1989@users.noreply.github.com>
2024-06-08 21:36:56 -0700

    Pushing Progress
    Updated the Script to allow me to drag a card around

--- Practice Card Game/Assets/Scripts/Draggable.cs
+++ Practice Card Game/Assets/Scripts/Draggable.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+    
+public void OnBeginDrag(PointerEventData eventData){
+    Debug.Log("OnBeginDrag");
+}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+public void OnDrag(PointerEventData eventData){
+    Debug.Log("OnDrag");
+
+    this.transform.position = eventData.position;
+}
+
+public void OnEndDrag(PointerEventData eventData){
+    Debug.Log("OnEndDrag");
+}
+
 }
